5431ef7f2a935d2ed801f66e63940e10d22e2c4f
Oliver <mb19626@bristol.ac.uk>
2020-04-12 16:45:55 +0100

    defines from comp

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -2,3 +2,11 @@
 
 
 #define SETMPVAR(var1,var2) missionNamespace SETPVAR_SYS(var1,var2)
+
+#define SCOPEL(var) scope = var; \
+                    scopeCurator = var; \
+                    scopeArsenal = var;
+
+#define PRIVATE			0
+#define PROTECTED		1
+#define PUBLIC			2
