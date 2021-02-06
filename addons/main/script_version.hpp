986b56b0663cbd20372b612f937f0a361fd0d5d1
OliverPennery <mb19626@bristol.ac.uk>
2021-02-06 18:28:11 +0000

    version

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 0
 #define MINOR 9
 #define PATCHLVL 6
-#define BUILD 0
+#define BUILD 1
--- tools/make.py
+++ tools/make.py
@@ -58,7 +58,7 @@ if sys.platform == "win32":
 
 ######## GLOBALS #########
 project = "@cav"
-project_version = "0.9.6.0"
+project_version = "0.9.6.1"
 arma3tools_path = ""
 work_drive = ""
 module_root = ""
